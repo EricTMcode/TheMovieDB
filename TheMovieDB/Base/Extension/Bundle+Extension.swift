46694bf9b57761c75e47a3314972400e8264b77a
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-28 15:58:23 +0200

    - Create Extension on Bundle for decoding JSON

---
+++ TheMovieDB/Base/Extension/Bundle+Extension.swift
@@ -0,0 +1,66 @@
+//
+//  Bundle+Extension.swift
+//  TheMovieDB
+//
+//  Created by Eric on 28/06/2023.
+//
+
+import Foundation
+
+// MARK: - BUNDLE EXTENSION
+
+extension Bundle {
+    
+    func decode<T: Codable>(filename: String) -> T {
+        // 1. Locate the JSON file
+        guard let url = self.url(forResource: filename, withExtension: "json") else {
+            fatalError("Failed to locate \(filename) in bundle.")
+        }
+        
+        // 2. Create a property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(filename) from bundle.")
+        }
+        
+        // 3. Create a decoder
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        // 4. Create a property for the decoded data
+        guard let decodedModel = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(filename) from bundle.")
+        }
+        
+        // 5. Return the ready-to-use data
+        return decodedModel
+    }
+}
+
+// MARK: - MOVIE EXTENSION
+
+extension Movie {
+    
+    // 1. Decode movie_list.json for development
+    static var localMovies: [Movie] {
+        let response: MovieResponse = Bundle.main.decode(filename: "movie_list")
+        return response.results
+    }
+    
+    // 2. Create an example movie for development
+    static var localMovie: Movie {
+        localMovies[0]
+    }
+    
+//    // 3. Decode movie_credits.json for development
+//    static var localCredits: [MovieCast] {
+//        let response: MovieCredit = Bundle.main.decode(filename: "movie_credits")
+//        return response.cast
+//
+//    }
+//
+//    // 4. Decode movie_similar.json for development
+//    static var localSimilar: [Movie] {
+//        let response: MovieSimilarResponse = Bundle.main.decode(filename: "movie_similar")
+//        return response.results
+//    }
+}
