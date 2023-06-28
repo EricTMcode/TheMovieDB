15fc4273296cb1d6ef4b949c538dc9700ed42d95
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-28 14:59:32 +0200

    Initial Commit

---
+++ TheMovieDB/TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBApp.swift
+//  TheMovieDB
+//
+//  Created by Eric on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
