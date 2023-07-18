3b2718ec73a34de63f07c1d08cb2c15a969a4818
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-18 14:48:30 +0200

    - Refactor PosterCard with extension

--- TheMovieDB/Features/MovieList/Components/PosterCard.swift
+++ TheMovieDB/Features/MovieList/Components/PosterCard.swift
@@ -11,42 +11,54 @@ struct PosterCard: View {
     let movie: Movie
     
     var body: some View {
-        VStack(alignment: .leading) {
-            ZStack {
-                RectangleView()
-                    
-                AsyncImage(url: movie.posterURL) { phase in
-                    if let image = phase.image {
-                        image
-                            .resizable()
-                            .scaledToFill()
-                    } else if phase.error != nil {
-                        Image(systemName: "video")
-                    } else {
-                        ProgressView()
-                    }
-                }
-            }
-            .cornerRadius(12)
-            .shadow(color: .black.opacity(0.15), radius: 5, x: 0, y: 5)
-            
-            VStack(alignment: .leading) {
-                Text(movie.title)
-                    .lineLimit(1)
-                    .font(.caption)
-                
-                Text("☆ \(movie.voteAverage, specifier: "%.1f")")
-                    .foregroundColor(.gray)
-                    .fontWeight(.bold)
-                    .font(.caption2)
-            }
-        }
-        .frame(width: 90, height: 150)
+        movieRowView(movie: movie)
+            .frame(width: 90, height: 150)
     }
 }
 
 struct PosterCard_Previews: PreviewProvider {
     static var previews: some View {
         PosterCard(movie: Movie.localMovie)
+    }
+}
+
+extension PosterCard {
+    func movieRowView(movie: Movie) -> some View {
+        VStack {
+            ZStack {
+                RectangleView()
+                moviePoster(movie: movie)
+            }
+            .cornerRadius(12)
+            .shadow(color: .black.opacity(0.15), radius: 5, x: 0, y: 5)
+            movieInfo(movie: movie)
+        }
+    }
+    
+    func moviePoster(movie: Movie) -> some View {
+        AsyncImage(url: movie.posterURL) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFill()
+            } else if phase.error != nil {
+                Image(systemName: "video")
+            } else {
+                ProgressView()
+            }
+        }
+    }
+    
+    func movieInfo(movie: Movie) -> some View {
+        VStack(alignment: .leading) {
+            Text(movie.title)
+                .lineLimit(1)
+                .font(.caption)
+            
+            Text("☆ \(movie.voteAverage, specifier: "%.1f")")
+                .foregroundColor(.gray)
+                .fontWeight(.bold)
+                .font(.caption2)
+        }
     }
 }
