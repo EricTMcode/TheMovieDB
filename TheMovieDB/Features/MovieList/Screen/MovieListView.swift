e14b3207cef602901352d39d7028e39f1f272751
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-10-15 15:29:09 +0200

    - Refactor MovieTvListView and TVListView

--- TheMovieDB/Features/MovieList/Screen/MovieListView.swift
+++ TheMovieDB/Features/MovieList/Screen/MovieListView.swift
@@ -12,13 +12,58 @@ struct MovieListView: View {
     
     
     var body: some View {
-        VStack {
-            ForEach(vm.topRated) { movie in
-                Text(movie.movieTitle)
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .leading, spacing: 10) {
+                    MediaBackdropCarouselView(content: vm.nowPlaying)
+                    MediaPosterCarouselView(title: "Movie of the day", content: vm.nowPlaying)
+                    MediaPosterCarouselView(title: "Recently Added", content: vm.upcoming)
+                    MediaPosterCarouselView(title: "Top Rated Movie", content: vm.topRated)
+                }
             }
-        }
-        .task {
-            await vm.populateMovies()
+            .navigationTitle("Welcome")
+            .task {
+                if !vm.hasAppeared {
+                    await vm.populateMovies()
+                    vm.hasAppeared = true
+                }
+            }
+            .refreshable {
+                Task {
+                    await vm.populateMovies()
+                }
+            }
+            .overlay {
+                if vm.isLoading {
+                    ProgressView()
+                }
+            }
+            .alert("Application Error", isPresented: $vm.hasError, presenting: vm.error) { _ in
+                Button("Retry") {
+                    Task {
+                        await vm.populateMovies()
+                    }
+                }
+            } message: { error in
+                Text(error.errorDescription ?? "Try again later")
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        Task {
+                            await vm.populateMovies()
+                        }
+                    } label: {
+                        Image(systemName: "arrow.counterclockwise")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(value: "SettingsView") {
+                        Image(systemName: "gear")
+                            .foregroundStyle(.orange)
+                    }
+                }
+            }
         }
     }
 }
--- TheMovieDB/Features/TVList/Screen/TVListView.swift
+++ TheMovieDB/Features/TVList/Screen/TVListView.swift
@@ -10,15 +10,53 @@ import SwiftUI
 struct TVListView: View {
     @StateObject var vm = TVListViewModel()
     
-    
     var body: some View {
-        VStack {
-            ForEach(vm.tvTopRated) { tv in
-                Text(tv.TVTitle)
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .leading, spacing: 10) {
+                    MediaBackdropCarouselView(content: vm.airingToday)
+                    MediaPosterCarouselView(title: "Top Rated", content: vm.tvTopRated)
+                    MediaPosterCarouselView(title: "On The Air", content: vm.onTheAir)
+                    MediaPosterCarouselView(title: "Popular", content: vm.popular)
+                }
             }
-        }
-        .task {
-            await vm.populateTV()
+            .navigationTitle("TV Shows")
+            .task {
+                if !vm.hasAppeared {
+                    await vm.populateTV()
+                    vm.hasAppeared = true
+                }
+            }
+            .refreshable {
+                Task {
+                    await vm.populateTV()
+                }
+            }
+            .overlay {
+                if vm.isLoading {
+                    ProgressView()
+                }
+            }
+            .alert("Application Error", isPresented: $vm.hasError, presenting: vm.error) { _ in
+                Button("Retry") {
+                    Task {
+                        await vm.populateTV()
+                    }
+                }
+            } message: { error in
+                Text(error.errorDescription ?? "Try again later")
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        Task {
+                            await vm.populateTV()
+                        }
+                    } label: {
+                        Image(systemName: "arrow.counterclockwise")
+                    }
+                }
+            }
         }
     }
 }
